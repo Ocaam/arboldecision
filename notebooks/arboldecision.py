3d136b2e54637a8286edf4228664f60b0d286f8b
Ocaam <pablo.oceja@cunef.edu>
2024-10-25 19:38:44 +0200

    arbol

---
+++ notebooks/arboldecision.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+import matplotlib.pyplot as plt
+import os
+
+print(os.getcwd())
+file_path = '../data/titanic.csv'
+
+
+
+
+
