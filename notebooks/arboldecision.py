af8c3b364ad0da985ec22a72b822e335d5442d2f
Ocaam <pablo.oceja@cunef.edu>
2024-10-25 20:28:29 +0200

    arboles

--- notebooks/arboldecision.py
+++ notebooks/arboldecision.py
@@ -1,14 +1,51 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn import tree
-import matplotlib.pyplot as plt
-import os
-
-print(os.getcwd())
-file_path = '../data/titanic.csv'
-
-
-
-
-
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "2024cef0-5182-466b-9cf5-faffa9bcbcb8",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "C:\\Users\\pablo\n"
+     ]
+    }
+   ],
+   "source": [
+    "import pandas as pd\n",
+    "from sklearn.model_selection import train_test_split\n",
+    "from sklearn.tree import DecisionTreeClassifier\n",
+    "from sklearn import tree\n",
+    "import matplotlib.pyplot as plt\n",
+    "import os\n",
+    "\n",
+    "print(os.getcwd())\n",
+    "file_path = '../data/titanic.csv'"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "árbol de decisión",
+   "language": "python",
+   "name": "arboldecision"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
